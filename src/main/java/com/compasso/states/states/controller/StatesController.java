5fb45bdc3806617392f28e28ff44efbf0f84cd12
Josiel <josielcardosomercio@gmail.com>
2021-09-05 17:00:27 -0300

    Update StatesController.java
    Completando método post cadastrar

--- src/main/java/com/compasso/states/states/controller/StatesController.java
+++ src/main/java/com/compasso/states/states/controller/StatesController.java
@@ -1,13 +1,17 @@
 package com.compasso.states.states.controller;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.compasso.states.states.controller.dto.StateDto;
 import com.compasso.states.states.controller.form.StateForm;
@@ -36,10 +40,14 @@ public class StatesController {
 	
 	
 	@PostMapping
-	public void cadastrar(StateForm form)
+	@Transactional
+	public ResponseEntity<StateDto> cadastrar(StateForm form, UriComponentsBuilder uriBuilder)
 	{
 		State state = form.converter();
 		stateRepository.save(state);
+		
+		URI uri = uriBuilder.path("/api/states/{id}").buildAndExpand(state.getId()).toUri();
+		return ResponseEntity.created(uri).body(new StateDto(state));
 	}
 
 }
