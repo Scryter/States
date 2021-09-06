e1f7946a0bb83bc2f76e8c83dc2fbcc6734bf8d8
Josiel <josielcardosomercio@gmail.com>
2021-09-05 21:16:46 -0300

    Update StatesController.java

--- src/main/java/com/compasso/states/states/controller/StatesController.java
+++ src/main/java/com/compasso/states/states/controller/StatesController.java
@@ -2,12 +2,14 @@ package com.compasso.states.states.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -57,19 +59,41 @@ public class StatesController {
 	}
 	
 	@GetMapping("/{id}")
-	public DetalhesDoStateDto detalhar(@PathVariable Long id)
+	public ResponseEntity<DetalhesDoStateDto> detalhar(@PathVariable Long id)
 	{
-		State state = stateRepository.getOne(id);
-		return new DetalhesDoStateDto(state);
+		
+		Optional<State> state = stateRepository.findById(id);
+		if (state.isPresent())
+		{
+			return ResponseEntity.ok(new DetalhesDoStateDto(state.get()));
+		}
+		return ResponseEntity.notFound().build();
 	}
 	
 	@PutMapping("/{id}")
 	@Transactional
 	public ResponseEntity<StateDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoStateForm form)
 	{
-		State state = form.atualizar(id, stateRepository);
-		
-		return ResponseEntity.ok(new StateDto(state));
+		Optional<State> optional = stateRepository.findById(id);
+		if (optional.isPresent())
+		{
+			State state = form.atualizar(id, stateRepository);
+			return ResponseEntity.ok(new StateDto(state));
+		}
+		return ResponseEntity.notFound().build();	
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> deletar (@PathVariable Long id)
+	{
+		Optional<State> state = stateRepository.findById(id);
+		if (state.isPresent())
+		{
+			stateRepository.deleteById(id);
+			return ResponseEntity.ok().build();
+		}
+		return ResponseEntity.notFound().build();
 	}
 
 }
